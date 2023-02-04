b1987b7e33337692efde981e1305f02f2fdda715
Aryah <ar341476@dal.ca>
2023-02-04 17:33:32 -0400

    shader material and vine imported

---
+++ Assets/Scripts/BridgeCreator.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeCreator : MonoBehaviour {
+    
+    // Start is called before the first frame update
+    private void Start() {
+        
+    }
+}
--- Assets/Scripts/PlayerScripts/MouseAim.cs
+++ Assets/Scripts/PlayerScripts/MouseAim.cs
@@ -7,13 +7,16 @@ namespace PlayerScripts {
         [SerializeField] private GameObject gun;
         [SerializeField] private Vector3 mousePosition;
         [SerializeField] private Vector3 mouseScreenPosition;
+        private Ray mouseRay;
         private Vector3 adjustedRay;
+        private float mid;
         private void Update() {
-            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            float mid = (gun.transform.position - Camera.main.transform.position).magnitude;
+            mousePosition = Input.mousePosition;
+            mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            mid = (gun.transform.position - Camera.main.transform.position).magnitude;
             gun.transform.LookAt(mouseRay.origin + mouseRay.direction * mid);
             // gun.transform.rotation = Quaternion.Euler(0, (Mathf.Rad2Deg * Mathf.Atan2(mousePosition.y - gun.transform.position.y, mousePosition.x - gun.transform.position.x)), 0);
             // gun.transform.LookAt(mouseScreenPosition, Vector3.right);
-        }
+        }   
     }
 }
