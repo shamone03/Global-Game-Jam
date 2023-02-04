9b80c360609a10f50fdfe607509f00b75c695cae
Aryah <ar341476@dal.ca>
2023-02-04 14:47:34 -0400

    adjust

--- Assets/Scripts/PlayerScripts/MouseAim.cs
+++ Assets/Scripts/PlayerScripts/MouseAim.cs
@@ -12,8 +12,6 @@ namespace PlayerScripts {
             Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             float mid = (gun.transform.position - Camera.main.transform.position).magnitude;
             gun.transform.LookAt(mouseRay.origin + mouseRay.direction * mid);
-            
-
             // gun.transform.rotation = Quaternion.Euler(0, (Mathf.Rad2Deg * Mathf.Atan2(mousePosition.y - gun.transform.position.y, mousePosition.x - gun.transform.position.x)), 0);
             // gun.transform.LookAt(mouseScreenPosition, Vector3.right);
         }
