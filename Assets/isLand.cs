f359fe5281a1041654240ba72190b8cdc02b4aa0
Simon-Losier <98567864+Simon-Losier@users.noreply.github.com>
2023-02-04 15:25:35 -0800

    Island

--- Assets/isLand.cs
+++ Assets/isLand.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class isLand : MonoBehaviour
 {
+    public bool isActive = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (isActive)
+        {
+            Activate();
+        }
+        else
+        {
+            Deactivate();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            Activate();
+            
+        }
+
+        if (Input.GetKey(KeyCode.B))
+        {
+            Deactivate();
+        }
+    }
+
+    private Material[] material;
+    void Activate() 
+    {
+        material = GetComponent<Renderer>().materials;
+        material[1].SetColor("_EmissionColor", Color.blue * Mathf.LinearToGammaSpace(2));
+    }
+
+    void Deactivate()
+    {
+        material = GetComponent<Renderer>().materials;
+        material[1].SetColor("_EmissionColor", Color.red * Mathf.LinearToGammaSpace(2));
     }
 }
