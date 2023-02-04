f9abd2f337f77cd49b7a6f46e48ee30aca5ea0d2
Aryah <ar341476@dal.ca>
2023-02-04 18:03:55 -0400

    creates vines on bridgecreator spawn

--- Assets/Scripts/BridgeCreator.cs
+++ Assets/Scripts/BridgeCreator.cs
@@ -1,12 +1,21 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BridgeCreator : MonoBehaviour {
+    [SerializeField] private GameObject root;
+
+    [SerializeField] private float length;
+    [SerializeField] private float width;
     
     // Start is called before the first frame update
     private void Start() {
         
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            GameObject rootObject = Instantiate(root, this.transform.position, Random.rotation); 
+            rootObject.transform.localScale *= length;
+        }
     }
 }
